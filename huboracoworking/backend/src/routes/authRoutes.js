e6e9d9bd1adde2ebf51ad37c52d30a361d3a2ab0
ChiquiLx <chiquilitob@hotmail.com>
2026-01-29 21:26:22 -0300

    added fx register

--- huboracoworking/backend/src/routes/authRoutes.js
+++ huboracoworking/backend/src/routes/authRoutes.js
@@ -1,11 +1,11 @@
 // se definen las urls
 import { Router } from "express";
-import { login } from "../controllers/authController.js";
+import { login, register } from "../controllers/authController.js";
 
 const router = Router();
 
-//post /api/login
-router.post("/register", register)
+//post /api/register
+router.post("/register", register);
 
 //post /api/login
 router.post("/login", login);
